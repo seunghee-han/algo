c9e26318cc55711fe46a5c3945e2335bcbe40700
SeungHee Han <54052702+seunghee-han@users.noreply.github.com>
2020-10-16 11:41:59 +0900

    2020-10-16

--- 소스.cpp
+++ 소스.cpp
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/*
 int factorial(int n) {
-	if (n > 0)
-		return n * factorial(n - 1);
+
+	printf("factorial 진입 n: %d\n", n);
+	if (n > 0){
+		int result = factorial(n - 1);
+		printf("factorial  진출 n: %d\n");
+		return n * result;
+}
 	else
 		return 1;
 }
@@ -11,6 +17,28 @@ int main(void) {
 	printf("정수를 입력하세요: ");
 	scanf_s("%d", &x);
 	printf("%d의 순차곱셈 값은 %d입니다.\n", x, factorial(x));
+
+	return 0;
+
+}
+*/
+
+int gcd(int x, int y) {
+	if (y == 0)
+		return x;
+	else
+		return gcd(y, x % y);
+}
+
+int main(void) {
+
+	int x, y;
+	puts("두 정수의 최대공약수를 구합니다.");
+	printf("정수를 입력하세요: ");
+	scanf_s("%d", &x);
+	printf("정수를 입력하세요: ");
+	scanf_s("%d", &y);
+	printf("최대공약수는 %d입니다. \n", gcd(x, y));
 
 	return 0;
 }
