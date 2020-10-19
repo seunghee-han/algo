e75c95a14b78263efc2851f2bf2a975092fbb6e7
SeungHee Han <54052702+seunghee-han@users.noreply.github.com>
2020-10-19 14:20:25 +0900

    선택정렬

--- 소스.c
+++ 소스.c
@@ -1,56 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define swap(type, x,y) do{type t=x;x=y;y=t;} while(0)
+/* 단순 선택 정렬
+// 비교횟수: (n^2-n)/2
+void selection(int a[], int n) {
 
-#define swap(type,x,y) do { type t = x; x = y; y = t; }while(0)
-
- 
-void bubble(int a[], int n) {
 	int i, j;
-	for(i = 0; i < n - 1; i++) {
-		for (j = n - 1; j > i; j--)
-			if (a[j - 1] > a[j])
-				swap(int, a[j - 1], a[j]);
-	}
-}
-/*
-* 6-3
-void bubble(int a[], int n)
-{
-	int k = 0;
-
-	while (k < n - 1) {
-		int j;
-		int last = n - 1;
-		for (j = n - 1; j > k; j--)
-			if (a[j - 1] > a[j]) {
-				swap(int, a[j - 1], a[j]);
-				last = j;
-			}
-		k = last;
+	for (i = 0; i < n - 1; i++) {
+		int min = i;
+		for (j = i + 1; j < n; j++)
+			if (a[j] < a[min])
+				min = j;
+		swap(int, a[i], a[min]);
 	}
 }
 */
 
-int main(void)
-{
+void insertion(int a[], int n) {
+	int i, j;
+	for (i = 1; i < n; i++) {
+		int tmp = a[i];
+		for (j = i; j > 0 && a[j - 1] > tmp; j--)
+			a[j] = a[j - 1];
+		a[j] = tmp;
+	}
+}
+
+int main(void) {
 	int i, nx;
 	int* x;
+	puts("단순 삽입 정렬");
 
-	puts("버블 정렬");
-	printf("요소 개수 : ");
-	scanf_s("%d", &nx);
+	printf("요소개수: ");
+	scnaf("%d", &nx);
 	x = calloc(nx, sizeof(int));
 
 	for (i = 0; i < nx; i++) {
-		printf("x[%d] : ", i);
-		scanf_s("%d", &x[i]);
+		printf("x[%d]: ", i);
+		scanf_s("%d", &x[i]); 
 	}
 
-	bubble(x, nx);
+	insertion(x, nx);
 
 	puts("오름차순으로 정렬했습니다.");
 	for (i = 0; i < nx; i++)
-		printf("x[%d] = %d\n", i, x[i]);
+		printf("x[%d]=%d\n", i, x[i]);
 
 	free(x);
 
