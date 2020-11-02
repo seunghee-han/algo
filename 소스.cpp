1a9b72d4573d897fc6b3eec06eb217c461791504
SeungHee Han <54052702+seunghee-han@users.noreply.github.com>
2020-11-02 15:14:05 +0900

    201102

--- 소스.cpp
+++ 소스.cpp
@@ -1,16 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define swap(type, x, y)do {type t = x; x =y; y = t;}while(0)
+#define swap(type, x, y) do {type t = x; x = y; y = t; }while(0)
 
-void partition(int a[], int n){
-	int i;
-	int pl = 0;
-	int pr = n - 1;
-	int x = a[n / 2];
-
+void quick(int a[], int left, int right){ 
+	int pl = left;
+	int pr = right;
+	int x = a[(pl + pr) / 2];
 	do {
-		while (a[pl] < x) pl++;
-		while (a[pr] > x) pr--;
+		while (a[pl] < x)pl++;
+		while (a[pr] > x)pr--;
 		if (pl <= pr) {
 			swap(int, a[pl], a[pr]);
 			pl++;
@@ -18,32 +16,15 @@ void partition(int a[], int n){
 		}
 
 	} while (pl <= pr);
-
-	printf("피벗의 값은 %d입니다.\n", x);
-	printf("피벗 이하의 그룹\n");
-	for (i = 0; i <= pl - 1; i++)
-		printf("%d", a[i]);
-	putchar('\n');
-
-	if (pl > pr + 1) {
-		printf("피벗과 일치하는 그룹\n");
-		for (i = pr + 1; i <= pl - 1; i++)
-			printf("%d", a[i]);
-		putchar('\n');
-	}
-
-	printf("피벗 이상의 그룹\n");
-	for (i = pr + 1; i < n; i++)
-		printf("%d ", a[i]);
-	putchar('\n');
+	if (left < pr) quick(a, left, pr);
+	if (pl < right) quick(a, pl, right);
 }
 
-int main()
-{
+int main(){
 	int i, nx;
 	int* x;
-	puts("배열을 나눕니다.");
-	printf("요소 개수 :  ");
+	puts("퀵 정렬");
+	printf("요소 개수 : ");
 	scanf_s("%d", &nx);
 	x = (int*)calloc(nx, sizeof(int));
 	for (i = 0; i < nx; i++) {
@@ -51,7 +32,10 @@ int main()
 		scanf_s("%d", &x[i]);
 	}
 
-	partition(x, nx);
+	quick(x, 0, nx - 1);
+	puts("오름차순으로 정렬했습니다.");
+	for (i = 0; i < nx; i++)
+		printf("x[%d] = %d\n", i, x[i]);
 	free(x);
 
 	return 0;
