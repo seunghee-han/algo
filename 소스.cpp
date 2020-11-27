23b0216d11f41e275fdbf562dd36abbcea48298c
SeungHee Han <54052702+seunghee-han@users.noreply.github.com>
2020-11-27 11:30:46 +0900

    1127(1)

--- 소스.cpp
+++ 소스.cpp
@@ -1,46 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void fsort(int a[], int n, int max) {
-	int i;
-	int *f = (int*)calloc(max + 1, sizeof(int));
-	int *b = (int*)calloc(n, sizeof(int));
-
-	for (i = 0; i < max; i++)f[i] = 0;
-	for (i = 0; i < n; i++)f[a[i]]++;
-	for (i = 1; i < max; i++)f[i] += f[i - 1];
-	for (i = n - 1; i >= 0; i--)b[--f[a[i]]] = a[i];
-	for (i = 0; i < n; i++)a[i] = b[i];
-
-	free(b);
-	free(f);
-}
-
-int main(void) {
-	int i, nx;
-	int* x;
-	const int max = 100;
-	puts("도수정렬");
-	printf("요소개수: ");
-
-	scanf_s("%d", &nx);
-	x = (int*)calloc(nx, sizeof(int));
-	printf("0~%d의 정수를 입력하세요.\n", max);
-
-	for (i = 0; i < nx; i++) {
-		do {
-			printf("x[%d]: ", i);
-			scanf_s("%d", &x[i]);
-		} while (x[i]<0 || x[i]>max);
+int str_cmp(const char *s1, const char* s2) {
+	while (*s1 == *s2) {
+		if (*s1 == '\0')
+			return 0;
+		s1++;
+		s2++;
 	}
-
-	fsort(x, nx, max);
-	puts("오름차순으로 정렬했습니다.");
-
-	for (i = 0; i < nx; i++)
-		printf("x[%d]=%d\n", i, x[i]);
-
-	free(x);
-
-	return 0;
+	return (unsigned char)*s1 - (unsigned char)*s2;
+}
+int main(void) {
+	char st[128];
+	puts("\"ABCD\"와 비교합니다.");
+	puts("\"XXXX\"면 종료합니다.");
+	while (1) {
+		printf("문지열 st: ");
+		scanf("%s", st);
+		if (str_cmp("XXXX", st) == 0)
+			break;
+		printf("str_cmp(\"ABCD\",st) = %d\n", str_cmp("ABCD", st));
+	}
+	return;
 }
