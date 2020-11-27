a363df4496a1788b9d38c10bd39a274acf919eda
SeungHee Han <54052702+seunghee-han@users.noreply.github.com>
2020-11-27 11:41:56 +0900

    1127(2)

--- 소스.cpp
+++ 소스.cpp
@@ -1,24 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
-int str_cmp(const char *s1, const char* s2) {
-	while (*s1 == *s2) {
-		if (*s1 == '\0')
-			return 0;
-		s1++;
-		s2++;
+int main() {
+	char name[10];
+
+	scanf("%s", name);
+
+	if (strcmp(name, "한승희") == 0) {
+		printf("%s, 안녕\n", name);
 	}
-	return (unsigned char)*s1 - (unsigned char)*s2;
-}
-int main(void) {
-	char st[128];
-	puts("\"ABCD\"와 비교합니다.");
-	puts("\"XXXX\"면 종료합니다.");
-	while (1) {
-		printf("문지열 st: ");
-		scanf("%s", st);
-		if (str_cmp("XXXX", st) == 0)
-			break;
-		printf("str_cmp(\"ABCD\",st) = %d\n", str_cmp("ABCD", st));
+	else {
+		printf("%s님, 안녕하세요\n",name)''
 	}
-	return;
 }
