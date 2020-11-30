6dc8eb588ada4f92e56fd8c0d08ef6b7d5303b9a
SeungHee Han <54052702+seunghee-han@users.noreply.github.com>
2020-11-30 16:45:06 +0900

    1130-3

--- 소스1.cpp
+++ 소스1.cpp
@@ -1,30 +1,18 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-
-int str_char(const char *s, int c){
-	int i = 0;
-	c=(char)c;
-	while(s[i] != c){
-		if(s[i] == '\0'
-		   return -1;
-		i++;
-	}
-	return i;
+void swap_ptr(char** x, char** y){
+	char* tmp = *x;
+	*x = *y;
+	*y = tmp;
 }
 
 int main(){
-	char str[256];
-	char tmp[64];
-	int ch;
-	int idx;
-	printf("문자열 : ");
-	scanf("%s",str);
-	printf("검색할 문자 : ");
-	scanf("%s",tmp);
-	ch = tmp[0];
-	if((idx = str_chr(str,ch)) == -1)
-		printf("문자 '%c'은(는) 문자열에 없습니다.\n",ch);
-	else
-		printf("문자 '%c'은(는) 문자열에 있습니다.\n",ch,idx+1);
+	char* s1 = (char*)"ABCD";
+	char* s2 = (char*)"EFGH";
+	printf("포인터 s1은 \"%s\"를 가리킵니다. \n", s1);
+	printf("포인터 s2은 \"%s\"를 가리킵니다. \n", s2);
+	swap_ptr(&s1, &s2);
+	puts("\n포인터 s1과 s2의 값을 서로 교환했습니다. \n");
+	printf("포인터 s1은 \"%s\"를 가리킵니다. \n", s1);
+	printf("포인터 s2은 \"%s\"를 가리킵니다. \n", s2);
 	return 0;
 }
