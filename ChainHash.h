bab9f4784c82cfdb5cb2ab31888bb1ca15e20177
SeungHee Han <54052702+seunghee-han@users.noreply.github.com>
2020-12-11 21:22:50 +0900

    Update and rename ChainHash.cpp to ChainHash.h

---
+++ ChainHash.h
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef ___ChainHash
+#define ___ChainHash
+#include "Member.h"
+
+typedef struct __node {
+	Member data;
+	struct __node* next;
+}Node;
+
+typedef struct {
+	int size;
+	Node** table;
+}ChainHash;
+
+int Initialize(ChainHash* h, int size);
+
+Node* Search(const ChainHash* h, int size);
+
+int Add(ChainHash* h, const Member* x);
+
+int Remove(ChainHash* h, const Member* x);
+
+void Dump(const ChainHash* h);
+
+void Terminate(ChainHash* h);
+#endif
